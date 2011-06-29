f3f3d880b7d2bd36c637130d1f32c4e13be404c4
Hell Fire <hellfire@servfire.net>
2011-06-29 22:40:18 +1000

    Well damn, where do I begin:
    - Added dynamically loaded menu list
    - Added cache for loaded menu (currently set for a week)
    
    Wow, that was a lot shorter than I thought :P Seemed like more code
    than that

--- extension/content/overlay.js
+++ extension/content/overlay.js
@@ -1,107 +1,13 @@
-var OCAU = {
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+var OCAUMenu = {
 
 	url: "http://www.overclockers.com.au/",
 	forum: "http://forums.overclockers.com.au/",
 	forumpage: "forumdisplay.php?f=",
 	
-	forums: [
-
-		{ name: "General Topics", number: 5, sub: [
-			{ name: "Newbie Lounge", number: 6 },
-			{ name: "Overclocking and Hardware", number: 7 },
-			{ name: "Troubleshooting Help", number: 8}
-		] },
-
-		{ name: "Specific Hardware Topics", number: 13, sub: [
-			{ name: "Modding", number: 23, sub: [
-				{ name:  "Worklogs and PCDB Entry Discussion", number: 52 }
-			] },
-			{ name: "Electronics", number: 73 },
-			{ name: "PC Audio", number: 50 },
-			{ name: "Extreme Cooling", number: 24 },
-			{ name: "Business and Enterprise Computing", number: 25 },
-			{ name: "Networking, Telephony and Internet", number: 26 },
-			{ name: "Video Cards and Monitors", number: 32 },
-			{ name: "Storage and Backup", number: 44 },
-			{ name: "Memory", number: 68 },
-			{ name: "Portable and Small Form Factor", number: 58 }
-		] },
-
-		{ name: "Manufacturer-specific Forums", number: 51, sub: [
-			{ name: "Intel Hardware", number: 21 },
-			{ name: "AMD Hardware", number: 22 },
-			{ name: "VIA Hardware", number: 28 },
-			{ name: "Apple Hardware and Software", number: 63 }
-		] },
-
-		{ name: "Software Topics", number: 38, sub: [
-			{ name: "Windows Operating Systems", number: 39 },
-			{ name: "Other Operating Systems", number: 40 },
-			{ name: "Graphics and Programming", number: 41 },
-			{ name: "Games", number: 42 },
-			{ name: "General Software", number: 9 }
-		] },
-
-		{ name: "Other Topics", number: 10, sub: [
-			{ name: "Motoring", number: 12, sub: [
-				{ name: "Technical", number: 55 },
-				{ name: "Worklogs", number: 53 },
-				{ name: "Marketplace", number: 71 },
-				{ name: "What/Where Should I Buy", number: 82 }
-
-			] },
-			{ name: "Geek Food", number: 30, sub: [
-				{ name: "Geek Recipes", number: 65 },
-				{ name: "Geek Grog", number: 72 }
-			] },
-			{ name: "Current Events and Serious Discussion", number: 31 },
-			{ name: "Career, Education and Finance", number: 60 },
-			{ name: "Lifestyle", number: 76 },
-			{ name: "Entertainment (TV, Movies, Music, Books, etc)", number: 49 },
-			{ name: "Musicians", number: 74, sub: [
-				{ name: "Marketplace", number: 81 },
-			] },
-			{ name: "Sport, Fitness and Health", number: 45, sub: [
-				{ name: "Journals", number: 56 },
-				{ name: "Major Sporting Events", number: 57 }
-			] },
-			{ name: "Photography", number: 48, sub: [
-				{ name: "The Gallery", number: 64 },
-				{ name: "Marketplace", number: 70 }
-			] },
-			{ name: "Audio Visual", number: 11 },
-			{ name: "Mobile Phones", number: 46 },
-			{ name: "Game Consoles", number: 61, sub: [
-				{ name: "Microsoft Consoles", number: 79 },
-				{ name: "Nintendo Consoles", number: 80 },
-				{ name: "Sony Consoles", number: 78 }
-			] },
-			{ name: "Pets and Animals", number: 67 },
-			{ name: "Other Toys", number: 27 },
-			{ name: "Science", number: 75 }
-		] },
-
-		{ name: "Shop and Swap", number: 14, sub: [
-			{ name: "For Sale - PC Related", number: 15 },
-			{ name: "For Sale - Non PC-Related", number: 77 },
-			{ name: "Wanted to Buy", number: 16 },
-			{ name: "Price Check", number: 33 },
-			{ name: "What/Where Should I Buy?", number: 18 },
-			{ name: "Sponsor Specials", number: 54 }
-		] },
-
-		{ name: "OCAU Community", number: 1, sub: [
-			{ name: "Site Discussion", number: 3, sub: [
-				{ name: "OCAU Podcast Discussion", number: 66 }
-			] },
-			{ name: "News", number: 34 },
-			{ name: "Team OCAU - Distributed Computing", number: 29 },
-			{ name: "The Pub", number: 4 },
-			{ name: "The Pool Room", number: 47 },
-			{ name: "TEST FORUM", number: 17 }
-		] }
-			
-	],
+	forums: new Array(),
 
 	makeLink: function(type, label, link) {
 		var item;
@@ -109,17 +15,17 @@ var OCAU = {
 		item.setAttribute('label', label);
 		if (link) {
 				item.link = link;
-				item.addEventListener('click', OCAU.openLinkEvent, false);
+				item.addEventListener('click', OCAUMenu.openLinkEvent, false);
 		}
 		return(item);
 	},
 
 	makeMenuItem: function(label, link) {
-		return(OCAU.makeLink('menuitem', label, link));
+		return(this.makeLink('menuitem', label, link));
 	},
 
 	makeMenu: function(label) {
-		return(OCAU.makeLink('menu', label));
+		return(this.makeLink('menu', label));
 	},
 
 	generateMenu: function(menu, menustruct) {
@@ -130,7 +36,7 @@ var OCAU = {
 				/* Has submenus */
 				temp = this.makeLink('menu', item.name, this.forum + this.forumpage + item.number);
 				tempPopup = document.createElement('menupopup');
-				OCAU.generateMenu(tempPopup, item.sub);
+				this.generateMenu(tempPopup, item.sub);
 				temp.appendChild(tempPopup);
 			} else {
 				/* Normal link */
@@ -139,37 +45,191 @@ var OCAU = {
 			menu.appendChild(temp);
 		}
 	},
-
-	onLoad: function() {
-		makeMenuItem = this.makeMenuItem;
-		makeMenu = this.makeMenu;
-		makeLink = this.makeLink;
-
-		forums = this.forums;
-		menu = document.getElementById('ocau-menu');
-		newmenu = document.createElement('menupopup');
-
+	
+	buildMenu: function() {
+		var newmenu = document.createElement('menupopup');
+		
 		/* Heading section, basic links */
-		newmenu.appendChild(makeMenuItem('Overclockers - Main Page', this.url));
-		newmenu.appendChild(makeMenuItem('Overclockers - Forums', this.forum));
-		newmenu.appendChild(makeMenuItem('Overclockers - Search Forums', this.forum + 'search.php'));
+		newmenu.appendChild(this.makeMenuItem('Overclockers - Main Page', this.url));
+		newmenu.appendChild(this.makeMenuItem('Overclockers - Forums', this.forum));
+		newmenu.appendChild(this.makeMenuItem('Overclockers - Search Forums', this.forum + 'search.php'));
 		newmenu.appendChild(document.createElement('menuseparator'));
-
-		OCAU.generateMenu(newmenu, forums);
-
+		
+		// Dynamic menu DOM build
+		this.generateMenu(newmenu, this.forums);
+		newmenu.appendChild(document.createElement('menuseparator'));
+		
 		/* User specific */
-		newmenu.appendChild(document.createElement('menuseparator'));
-		userMenu = makeMenu('User Control Panel');
-		userPopup = document.createElement('menupopup');
-		userPopup.appendChild(makeMenuItem('User CP', this.forum + 'usercp.php'));
-		userPopup.appendChild(makeMenuItem('List Private Messages', this.forum + 'private.php'));
-		userPopup.appendChild(makeMenuItem('List Subscriptions', this.forum + 'subscription.php?do=viewsubscription'));
+		var userMenu = this.makeMenu('User Control Panel');
+		var userPopup = document.createElement('menupopup');
+		userPopup.appendChild(this.makeMenuItem('User CP', this.forum + 'usercp.php'));
+		userPopup.appendChild(this.makeMenuItem('List Private Messages', this.forum + 'private.php'));
+		userPopup.appendChild(this.makeMenuItem('List Subscriptions', this.forum + 'subscription.php?do=viewsubscription'));
 		userMenu.appendChild(userPopup);
 		newmenu.appendChild(userMenu);
+		
+		return newmenu;
+	},
+	
+	setupMenus: function() {
+		// Replace the default menu
+		var menuElem = document.getElementById('ocau-menu');
+		if (menuElem) {
+			menuElem.replaceChild(this.buildMenu(), menuElem.firstChild);
+		}
+		var button = document.getElementById('ocau-button');
+		if (button) {
+			button.replaceChild(this.buildMenu(), button.firstChild);
+		}
+	},
+	
+	log: function(message) {
+		var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
+				.getService(Components.interfaces.nsIConsoleService);
+		consoleService.logStringMessage("OCAUMenu: " + message);
+	},
+	
+	init: function() {
 
-		menu.replaceChild(newmenu, menu.firstChild);
+		if (!this.initialized) {
+			OCAUMenu.log("OCAUMenu initializing...")
+			this.setupMenus();
 
+			this.cacheFile = FileUtils.getFile('ProfD', ['ocauMenu.cache']);
+			try {
+				// TODO: this might race here, something to keep in mind
+				this.readFile(this.cacheFile, OCAUMenu.loadFile);
+			} catch (e) {
+				OCAUMenu.log("Couldn't fetch ocauMenu.cache: " + e.name + " - " + e.message);
+				this.doHTTPRequest();
+			}
+		}
+	},
+	
+	loadFile: function(inputStream, result) {
+		if (Components.isSuccessCode(result)) {
+			var data = JSON.parse(NetUtil.readInputStreamToString(inputStream, inputStream.available()));
+			if (data) {
+				OCAUMenu.log("File loaded okay");
+				data.lastUpdated = new Date(data.lastUpdated);
+				var aWeekAgo = new Date();
+				aWeekAgo.setDate(aWeekAgo.getDate()-7);
+				if (data.lastUpdated > aWeekAgo) {
+					OCAUMenu.log("Using data from file");
+					OCAUMenu.forums = data.forums;
+					OCAUMenu.setupMenus.apply(OCAUMenu);
+					OCAUMenu.initialized = true;
+					return;
+				}
+			}
+		}
+		OCAUMenu.doHTTPRequest.apply(OCAUMenu);
+	},
+	
+	doHTTPRequest: function() {
+		OCAUMenu.log("Sending HTTP Request");
+		this.request = new XMLHttpRequest();
+		this.request.open('GET', this.forum + 'index.php?styleid=1', true);
+		this.request.onreadystatechange = OCAUMenu.updateMenu;
+		this.request.send();
 		this.initialized = true;
+	},
+	
+	/*
+	 * Callbacks below, unknown reference objects, beware pirates, etc
+	 */
+	updateMenu: function() {
+		// All this code is pretty specific to OCAU and probably VB3, so is likely to break elsewhere or on changes
+		// This is a XMLHttpRequest callback, so this isn't our object
+		if (OCAUMenu.request.readyState == 4 && (OCAUMenu.request.status == 200 || OCAUMenu.request.status == 0)) {
+			OCAUMenu.log("Loaded data from HTTP - " + OCAUMenu.request.status);
+			/*  Forums have 3 main types
+			*	- Categories
+			*	- Forums
+			*	- Subforums
+			*/
+			var text = OCAUMenu.request.responseText;
+			
+			text = text.match(/.*<a href="forumdisplay.php?.*<\/a>/g); // Find the forum lines we're interested in
+			
+			var newForums = new Array();
+			var category = null;
+			var forum = null;
+			for (var lineNum = 0; lineNum < text.length; ++lineNum) {
+				// Categories are the plainest looking
+				// Forums have <strong> around their names
+				// Subforums are chucked in on a single line
+				// Check in reverse order makes it easy
+				if (text[lineNum].match(/<strong>Sub-Forums<\/strong>:/)) {
+					if (typeof(forum.sub) == "undefined") { forum.sub = new Array(); }
+					var subs = text[lineNum].match(/f=[0-9]*">.*?<\/a>/g);
+					for (var i = 0; i < subs.length; ++i) {
+						var cur = subs[i].match(/f=([0-9]*)">(.*)<\/a>/);
+						var name = cur[2].replace(/&amp;/gi, "&");
+						var number = cur[1];
+						forum.sub.push({ name: name, number: number });
+					}
+				} else {
+					var cur = text[lineNum].match(/f=([0-9]*)">(<strong>)?(.*?)(<\/strong>)?<\/a>/);
+					if (cur) { // We have a forum! (ignore other forumdisplay.php links without a forum)
+						var name = cur[3].replace(/&amp;/gi, "&");
+						var number = cur[1];
+						if (!cur[2]) { // Is a category (doesn't have <strong> tag
+							if (category) { newForums.push(category); }
+							category = { name: name, number: number };
+						} else { // Is a forum
+							if (typeof(category.sub) == "undefined") { category.sub = new Array(); }
+							if (forum) {
+								category.sub.push(forum);
+							}
+							forum = { name: name, number: number };
+						}
+					}
+				}
+
+			}
+			// Push the last forum and category into the new menu
+			if (forum) {
+				category.sub.push(forum);
+				forum = null;
+			}
+			if (category) {
+				newForums.push(category);
+				category = null;
+			}
+			
+			OCAUMenu.log("Parsed data, updating!");
+			
+			OCAUMenu.forums = newForums;
+			
+			// Update the DOM elements
+			OCAUMenu.setupMenus.apply(OCAUMenu);
+			
+			try {
+				OCAUMenu.log("Writing to cache");
+				var now = new Date();
+				OCAUMenu.writeFile(OCAUMenu.cacheFile, JSON.stringify({ forums: OCAUMenu.forums, lastUpdated: now.getTime() }));
+			} catch (e) {
+				OCAUMenu.log("Couldn't write to file: " + e.name + " - " + e.message);
+			}
+		}
+	},
+	
+	readFile: function(file, callback) {
+		NetUtil.asyncFetch(file, callback);
+	},
+	
+	writeFile: function(file, inString) {
+		var ostream = FileUtils.openSafeFileOutputStream(file);
+		var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
+			createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+		converter.charset = "UTF-8";
+		var istream = converter.convertToInputStream(inString);
+		NetUtil.asyncCopy(istream, ostream);
+	},
+
+	onLoad: function() {
+		OCAUMenu.init.apply(OCAUMenu);
 	},
 
 	openLinkEvent: function(event) {
@@ -188,5 +248,6 @@ var OCAU = {
 	}
 };
 
-window.addEventListener("load", function(e) { OCAU.onLoad(e); }, false); 
+window.addEventListener("DOMContentLoaded", OCAUMenu.onLoad, false);
+window.addEventListener("aftercustomization", OCAUMenu.onLoad, false);
 
